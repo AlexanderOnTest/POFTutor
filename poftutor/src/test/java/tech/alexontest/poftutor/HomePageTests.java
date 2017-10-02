ae01b65a445f5da0bf71f320d234b75dd0c3da5c
Alexander Dunn <alex.dunn@experian.com>
2017-10-02 02:23:05 +0100

    Final tweaks (DisplayNames on HomePage Tests) Added commands to set up my grid

--- poftutor/src/test/java/tech/alexontest/poftutor/HomePageTests.java
+++ poftutor/src/test/java/tech/alexontest/poftutor/HomePageTests.java
@@ -1,6 +1,7 @@
 package tech.alexontest.poftutor;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import tech.alexontest.poftutor.infrastructure.AbstractTest;
@@ -13,17 +14,18 @@ class HomePageTests extends AbstractTest {
 
     @BeforeEach
     void setupHomepage() {
-        //homePage = new PomHomePage("https://alexanderontesting.com/");                            //Classic implementation
         homePage = new PfHomePage(getDriver("https://alexanderontesting.com/"));                             //Page Factory implementation
     }
 
     @Test
+    @DisplayName("The correct title is reported")
     void titleIsCorrect() {
         assertThat(homePage.getPageTitle())
                 .isEqualToIgnoringCase("Alexander on Testing");
     }
 
     @Test
+    @DisplayName("5 Widgets appear on the Homepage")
     void pageContainsFiveWidgets() {
         assertThat(homePage.getWidgets())
                 .size()
@@ -31,6 +33,7 @@ class HomePageTests extends AbstractTest {
     }
 
     @Test
+    @DisplayName("No more than 5 posts are linked on the Homepage")
     void pageContainsUpToFiveArticles() {
         assertThat(homePage.getArticles())
                 .size()
