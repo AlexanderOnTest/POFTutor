034450e785b7e82c343d85fbc833f52ee4b8db19
Alexander Dunn <alexander@alexanderontesting.com>
2018-01-30 01:09:58 +0000

    Files for easy grid configuration
    30/01/18
    One command to launch the grid.

---
+++ poftutor/grid/Start Grid.ps1
@@ -0,0 +1,3 @@
+﻿Start-Process powershell {java -jar "C:\Grid\selenium-server-standalone-3.8.1.jar" -role hub}
+
+Start-Process powershell {java -jar "C:\Grid\selenium-server-standalone-3.8.1.jar" -port 5560 -role node -hub http://localhost:4444/grid/register -nodeConfig "C:\Grid\nodeconfig.json"}
