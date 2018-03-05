7149820de76d7b9a3fca766c232787412ec08a40
Alexander Dunn <alexander@alexanderontesting.com>
2018-03-05 00:03:44 +0000

    Use BlockingElementLocatorFactory for all Blocks
    Added a constructor matching the signature and functionality of the
    DefaultElementLocatorFactory and add handling in case user error uses
    the wrong one.

--- poftutor/src/main/java/tech/alexontest/poftutor/infrastructure/pagefactory/BlockingElementLocatorFactory.java
+++ poftutor/src/main/java/tech/alexontest/poftutor/infrastructure/pagefactory/BlockingElementLocatorFactory.java
@@ -1,6 +1,7 @@
 package tech.alexontest.poftutor.infrastructure.pagefactory;
 
 import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.pagefactory.DefaultElementLocator;
 import org.openqa.selenium.support.pagefactory.ElementLocator;
 import org.openqa.selenium.support.pagefactory.ElementLocatorFactory;
@@ -9,23 +10,42 @@ import tech.alexontest.poftutor.pageblocks.Block;
 import java.lang.reflect.Field;
 
 public class BlockingElementLocatorFactory implements ElementLocatorFactory {
-    private final SearchContext driver;
+    private final SearchContext searchContext;
 
     private final Field rootElementField;
 
-    public BlockingElementLocatorFactory(final SearchContext driver, final Block block) {
-        this.driver = driver;
+    /**
+     * Instantiate a new ElementLocatorFactory that is functionally equivalent to the DefaultElementLocatorFactory.
+     * @param searchContext the SearchContext to use as the rootElement of the Block. Typically a 'found' WebElement.
+     */
+    public BlockingElementLocatorFactory(final SearchContext searchContext) {
+        this.searchContext = searchContext;
+        rootElementField = null;
+    }
+
+    /**
+     * Instantiate a new BlockingLocatorFactory that sets up a block using PageFactory annotations.
+     * The rootElement of the block is defined by @FindBy annotation on the 'rootElement' field of the Block parameter.
+     * @param driver A SearchContext (Typically WebDriver)
+     * @param definedBlock A Block class where the rootElement field has a PageFactory @FindBy annotation
+     */
+    public BlockingElementLocatorFactory(final SearchContext driver, final Block definedBlock) {
+        this.searchContext = driver;
         try {
-            this.rootElementField = block.getClass().getDeclaredField("rootElement");
+            rootElementField = definedBlock.getClass().getDeclaredField("rootElement");
+            // This constructor should only be used for Blocks with an annotated rootElement
+            if (rootElementField.getAnnotationsByType(FindBy.class).length == 0) {
+                throw new IllegalArgumentException("The 'rootElement' field does not have an @FindBy annotation.");
+            }
         } catch (final NoSuchFieldException e) {
-            throw new IllegalArgumentException(e);
+            throw new IllegalArgumentException("A defined Block requires a @FindBy annotated 'rootElement' field.", e);
         }
     }
 
     @Override
     public ElementLocator createLocator(final Field field) {
-        return (field.getName().equals("rootElement"))
-                ? new DefaultElementLocator(driver, field)
-                : new BlockingElementLocator(driver, rootElementField, field);
+        return (rootElementField == null || field.getName().equals("rootElement"))
+                ? new DefaultElementLocator(searchContext, field)
+                : new BlockingElementLocator(searchContext, rootElementField, field);
     }
 }
--- poftutor/src/main/java/tech/alexontest/poftutor/pageblocks/AbstractFoundBlock.java
+++ poftutor/src/main/java/tech/alexontest/poftutor/pageblocks/AbstractFoundBlock.java
@@ -2,15 +2,15 @@ package tech.alexontest.poftutor.pageblocks;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.pagefactory.DefaultElementLocatorFactory;
 import org.openqa.selenium.support.pagefactory.ElementLocatorFactory;
+import tech.alexontest.poftutor.infrastructure.pagefactory.BlockingElementLocatorFactory;
 
 public abstract class AbstractFoundBlock implements Block {
 
     private final WebElement rootElement;
 
     AbstractFoundBlock(final WebElement rootElement) {
-        final ElementLocatorFactory locatorFactory = new DefaultElementLocatorFactory(rootElement);
+        final ElementLocatorFactory locatorFactory = new BlockingElementLocatorFactory(rootElement);
         PageFactory.initElements(locatorFactory, this);
         this.rootElement = rootElement;
     }
