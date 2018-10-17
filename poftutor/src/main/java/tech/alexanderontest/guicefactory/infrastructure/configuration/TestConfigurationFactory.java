cb5c3c49ee7b00d660439decfb660511b563afbb
Alexander Dunn <alexander@alexanderontesting.com>
2018-10-17 01:11:54 +0100

    Add Copyright to main source

--- poftutor/src/main/java/tech/alexanderontest/guicefactory/infrastructure/configuration/TestConfigurationFactory.java
+++ poftutor/src/main/java/tech/alexanderontest/guicefactory/infrastructure/configuration/TestConfigurationFactory.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2018. Alexander Dunn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package tech.alexanderontest.guicefactory.infrastructure.configuration;
 
 import com.google.common.base.Charsets;
@@ -35,7 +51,6 @@ public final class TestConfigurationFactory {
             final URL configFile = (userConfig.exists() && !userConfig.isDirectory())
                     ? userConfig.toURI().toURL()
                     : Resources.getResource("config.json");
-            //TODO check config file exists and apply default defaults and log message.
             fileConfiguration = new Gson().fromJson(
                     Resources.toString(configFile, Charsets.UTF_8),
                     TestConfigurationImpl.class);
