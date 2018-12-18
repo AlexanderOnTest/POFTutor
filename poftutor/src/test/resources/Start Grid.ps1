fd5ffc0481a4def0c0e317c2d66ab815d2a9fe25
Alexander Dunn <alexander@alexanderontesting.com>
2018-12-18 22:10:54 +0000

    Updates

--- poftutor/src/test/resources/Start Grid.ps1
+++ poftutor/src/test/resources/Start Grid.ps1
@@ -1,3 +1,3 @@
-﻿Start-Process powershell {java -jar "C:\Grid\selenium-server-standalone-3.12.0.jar" -role hub}
+﻿Start-Process powershell {java -jar ".\selenium-server-standalone-3.14.0.jar" -role hub}
 
-Start-Process powershell {java -jar "C:\Grid\selenium-server-standalone-3.12.0.jar" -port 5560 -role node -hub http://localhost:4444/grid/register -nodeConfig "C:\Grid\nodeconfig.json"}
+Start-Process powershell {java -jar ".\selenium-server-standalone-3.14.0.jar" -port 5560 -role node -hub http://localhost:4444/grid/register -nodeConfig ".\nodeconfig.json"}
